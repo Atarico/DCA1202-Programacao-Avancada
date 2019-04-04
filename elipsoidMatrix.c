d9027649f2f698c827ae896f15c7f485498e2ec2
Luiz Paulo <luiz_paulo.09@hotmail.com>
2019-04-04 10:40:49 -0300

    Changed the matrix generation type.
    Changed the matrix generation, from multiple mallocs inside two for statements, to three single mallocs (representing the matrix dimmension, 3x3), which is better for performance.

--- elipsoidMatrix.c
+++ elipsoidMatrix.c
@@ -2,48 +2,66 @@
 #include <stdlib.h>
 #include <math.h>
 
-unsigned char *** generateMatrix(int nl, int nc, int np);
-void printMatrix(int nl, int nc, int np, unsigned char *** matrix);
-void freeAllocMatrix(int nl, int nc, int np, unsigned char *** matrix);
-void drawElipsoidOnMatrix(int nl, int nc, int np, unsigned char *** matrix, int x0, int y0, int z0);
+int *** generateMatrix(int nl, int nc, int np);
+int *** generateMatrix2(int nl, int nc, int np);
+void printMatrix(int nl, int nc, int np, int *** matrix);
+void printMatrix2(int nl, int nc, int np, int *** matrix);
+void freeAllocMatrix(int *** matrix);
+void drawElipsoidOnMatrix(int nl, int nc, int np, int *** matrix, int x0, int y0, int z0);
 
 int main()
 {
-    int nl=13, nc=13, np=13;
+    int nl=9, nc=9, np=9;
     unsigned char ***space3D;
 
     space3D = generateMatrix(nl, nc, np);
-//  printMatrix(nl, nc, np, space3D);
-    drawElipsoidOnMatrix(nl, nc, np, space3D, 6, 6, 6);
+//    printMatrix(nl, nc, np, space3D);
+    drawElipsoidOnMatrix(nl, nc, np, space3D, 4, 5, 3);
     printMatrix(nl, nc, np, space3D);
-//  freeAllocMatrix(nl, nc, np, space3D);
+    freeAllocMatrix(space3D);
 
     return 0;
 }
 
+int *** generateMatrix(int nl, int nc, int np){
+    int i, j, k;
+    int ***space3D;
 
-unsigned char *** generateMatrix(int nl, int nc, int np){
-    int i, j;
-    unsigned char ***space3D;
+    space3D = malloc(np*sizeof(int**));
+    space3D[0] = malloc(np*nl*sizeof(int*));
+    space3D[0][0] = malloc(np*nl*nc*sizeof(int));
 
-    space3D = (unsigned char***) malloc(np*sizeof(unsigned char**));
-
-    for( i = 0; i < np; i++){
-        space3D[i] = (unsigned char**) malloc(nl*sizeof(unsigned char*));
-        for(j = 0; j<nl; j++){
-            space3D[i][j] = (unsigned char*) calloc(1,nc*sizeof(unsigned char));
+    for(i = 1; i<np; i++){
+        space3D[i] = space3D[i-1] + nl;
+    }
+    for(i = 0; i<np; i++){
+        for(j=0; j<nl; j++){
+            if(j != 0){
+                space3D[i][j] = space3D[i][j-1] + (nc);
+            }
+            else if(i!=0 && j==0){
+                space3D[i][j] = space3D[i-1][nl-1] + (nc);
+            }
+        }
+    }
+    for(i=0; i<np; i++){
+        for(j=0; j<nl; j++){
+            for(k=0; k<nc; k++){
+                space3D[i][j][k] = 0;
+            }
         }
     }
     return space3D;
 }
 
-void printMatrix(int nl, int nc, int np, unsigned char *** matrix){
+void printMatrix(int nl, int nc, int np, int *** matrix){
     int i, j, k;
 
+    printf("BEGGIN OF MATRIX PRINTING: \n");
     for( i = 0; i < np; i++){
        for( j = 0; j < nl; j++){
            for( k = 0; k < nc; k++){
-               printf("%d", matrix[i][j][k]);
+               printf("%d ", matrix[i][j][k]);
            }
            printf("\n");
        }
@@ -52,28 +70,23 @@ void printMatrix(int nl, int nc, int np, unsigned char *** matrix){
        }
        printf("\n");
     }
+    printf("END OF MATRIX PRINTING \n\n\n\n");
 }
 
-void freeAllocMatrix(int nl, int nc, int np, unsigned char *** matrix){
-    int i, j, k;
-
-    for( i = 0; i < np; i++){
-       for( j = 0; j < nl; j++){
-           for( k = 0; k < nc; k++){
-               free(&matrix[i][j][k]);
-           }
-       }
-    }
+void freeAllocMatrix(int *** matrix){
+    free(matrix);
+    free(matrix[0]);
+    free(matrix[0][0]);
 }
 
-void drawElipsoidOnMatrix(int nl, int nc, int np, unsigned char *** matrix, int x0, int y0, int z0){
+void drawElipsoidOnMatrix(int nl, int nc, int np, int *** matrix, int x0, int y0, int z0){
     int i; int j; int k;
 
     for(i = 0; i < np; i++){
         for(j = 0; j < nl; j++){
             for(k = 0; k < nc; k++){
                 if(
-                    (pow((k-z0),2) / pow(nc/2, 2) + pow((j-y0),2) / pow(nl/2, 2) + pow((i-x0),2) / pow(np/2, 2)) <= 1
+                    (pow((k-z0),2) / (float)pow(nc/2, 2) + pow((j-y0),2) / (float)pow(nl/2, 2) + pow((i-x0),2) / (float)pow(np/2, 2)) <= 1
                 ){
                     matrix[i][j][k] = 1;
                 }
@@ -81,4 +94,3 @@ void drawElipsoidOnMatrix(int nl, int nc, int np, unsigned char *** matrix, int 
         }
     }
 }
-
