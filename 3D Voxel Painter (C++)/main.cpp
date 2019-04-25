ab4233d85682ebadb186004a7d145b7821e1eada
Luiz Paulo <luiz_paulo.09@hotmail.com>
2019-04-25 16:17:55 -0300

    maincpp for testing writeVECT and writeOFF

--- 3D Voxel Painter (C++)/main.cpp
+++ 3D Voxel Painter (C++)/main.cpp
@@ -12,30 +12,38 @@ int main()
     //cout<<"Digite, em sequencia, as dimensoes do seu canvas (x, y, z): "<<endl;
     //cin>>nx>>ny>>nz;
 
-    Sculptor escultor(v, 9, 9, 9);
+    Sculptor escultor(v, 2, 2, 2);
 
     //cout<<"Digite, em sequencia, os valores rgb e o alpha: "<<endl;
     //cin>>r>>g>>b>>a;
 
     escultor.setColor(1, 0, 0, 1);
     //escultor.print();
-    cout<<"---------------------"<<endl;
-    escultor.putEllipsoid(4,4,4,2,4,2);
-    escultor.print();
-    cout<<"---------------------"<<endl;
-    escultor.cutEllipsoid(4,4,4,1,2,1);
-    escultor.print();
-
-    //escultor.putBox(0,1,0,1,0,1);
-    //escultor.cutVoxel(0,0,0);
-    //escultor.cutVoxel(1,1,1);
-    //escultor.setColor(0, 1, 0, 1);
-    //escultor.putVoxel(1,1,1);
+    //cout<<"---------------------"<<endl;
+    //escultor.putEllipsoid(4,4,4,2,4,2);
+    //escultor.print();
+    //cout<<"---------------------"<<endl;
+    //escultor.cutEllipsoid(4,4,4,1,2,1);
     //escultor.print();
 
-    string str("VECT_file");
+    escultor.putBox(0,1,0,1,0,1);
+    escultor.cutVoxel(0,0,0);
+    //escultor.cutVoxel(1,1,1);
+    escultor.setColor(0, 1, 0, 1);
+    escultor.putVoxel(1,1,1);
+    escultor.print();
 
-    escultor.writeVECT(str);
+
+    Sculptor escultor2(v, 1, 1, 1);
+    escultor2.setColor(1, 1, 1, 1);
+    escultor2.putVoxel(0,0,0);
+    escultor2.print();
+
+    string vect_scr("VECT_file");
+    string off_scr("OFF_file");
+
+    escultor.writeVECT(vect_scr);
+    escultor2.writeOFF(off_scr);
 
     return 0;
 }
