adf586babd53dfc3db8f1f54e4cc08be09637e33
Luiz Paulo <luiz_paulo.09@hotmail.com>
2019-05-01 22:26:04 -0300

    Updating the testing main file

--- 3D Voxel Painter (C++)/main.cpp
+++ 3D Voxel Painter (C++)/main.cpp
@@ -11,49 +11,40 @@ int main()
     //cout<<"Digite, em sequencia, as dimensoes do seu canvas (x, y, z): "<<endl;
     //cin>>nx>>ny>>nz;
 
-    Sculptor escultor(3, 2, 2);
-
     //cout<<"Digite, em sequencia, os valores rgb e o alpha: "<<endl;
     //cin>>r>>g>>b>>a;
 
-    escultor.setColor(1, 0, 0, 1);
-    //escultor.print();
-    //cout<<"---------------------"<<endl;
-    //escultor.putEllipsoid(4,4,4,2,4,2);
-    //escultor.print();
-    //cout<<"---------------------"<<endl;
-    //escultor.cutEllipsoid(4,4,4,1,2,1);
-    //escultor.print();
+    //Declarando o objeto escultor1, usado como exemplo para escrever o arquivo .vect
+    Sculptor escultor1(3, 2, 2);
 
-    escultor.putBox(0,1,0,1,0,1);
-    escultor.cutVoxel(0,0,0);
-    //escultor.cutVoxel(1,1,1);
-    escultor.setColor(0, 1, 0, 1);
-    escultor.putVoxel(1,1,1);
-    escultor.print();
-
-
-//    Sculptor escultor2(2, 2, 2);
-//    escultor2.setColor(1, 0, 0, 1);
-//    escultor2.putVoxel(0,0,0);
-//    escultor2.setColor(0, 1, 0, 1);
-//    escultor2.putVoxel(0,1,0);
-//    escultor2.setColor(0, 0, 1, 1);
-//    escultor2.putVoxel(0,0,1);
-//    escultor2.setColor(0.85f, 0, 0.85f, 0.85f);
-//    escultor2.putVoxel(1,0,1);
-//    escultor2.print();
-
-    Sculptor escultor2(13,13,13);
-    escultor2.setColor(0,1,0,0.7);
-    escultor2.putSphere(6,6,6,4);
-    escultor2.cutSphere(6,6,6,3);
-    escultor2.cutBox(0,4,0,13,0,13);
+    escultor1.setColor(1, 0, 0, 1);
+    escultor1.putBox(0,1,0,1,0,1);
+    escultor1.cutVoxel(0,0,0);
+    escultor1.setColor(0, 1, 0, 1);
+    escultor1.putVoxel(1,1,1);
 
     string vect_scr("VECT_file");
-    string off_scr("OFF_file");
+    escultor1.writeVECT(vect_scr);
 
-    escultor.writeVECT(vect_scr);
+
+    //Declarando o objeto escultor2 usado como exemplo para escrever o arquivo .off
+    Sculptor escultor2(25,35,25);
+
+    escultor2.setColor(0,1,0,0.7);
+    escultor2.putEllipsoid(12,16,12,12,16,12);
+    escultor2.cutEllipsoid(12,16,12,11,15,11);
+    escultor2.setColor(1,1,1,0.5);
+    escultor2.putSphere(6,16,8,4);
+    escultor2.cutSphere(6,16,8,3);
+    escultor2.putSphere(6,16,16,4);
+    escultor2.cutSphere(6,16,16,3);
+    escultor2.setColor(0,0,0,1);
+    escultor2.putSphere(6,16,8,2);
+    escultor2.putSphere(6,16,16,2);
+    escultor2.cutBox(0,4,0,25,0,25);
+
+
+    string off_scr("OFF_file");
     escultor2.writeOFF(off_scr);
 
     return 0;
