9c45b98807318c4428a4342450a5db97e589b01d
Marcos Matsunaga <47721533+matsunaga87@users.noreply.github.com>
2019-05-02 15:42:33 -0300

    Add files via upload
    add doxygen n

---
+++ Nova pasta/html/search/functions_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['operator_28_29',['operator()',['../class_sculptor.html#a772a46cc6396053f7af669ecc29439d6',1,'Sculptor']]]
+];
---
+++ Nova pasta/html/search/functions_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['sculptor',['Sculptor',['../class_sculptor.html#a014e3ef5517bf0e9d9e14486b6ac6433',1,'Sculptor']]],
+  ['setcolor',['setColor',['../class_sculptor.html#a77ed7894185001ca9c8d3700c89bd245',1,'Sculptor']]]
+];
