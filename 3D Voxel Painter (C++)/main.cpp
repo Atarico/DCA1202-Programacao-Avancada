3f84e88b09cb49f5462130165c3badcace9fa6f2
matsunaga87 <marcos.matsunaga@uol.com.br>
2019-05-17 12:42:58 -0300

    add modification in main.cpp

--- 3D Voxel Painter (C++)/main.cpp
+++ 3D Voxel Painter (C++)/main.cpp
@@ -7,6 +7,12 @@
 #include "geometricfigure.h"
 #include "putbox.h"
 #include "cutbox.h"
+#include "putsphere.h"
+#include "cutsphere.h"
+#include "putellipsoid.h"
+#include "cutellipsoid.h"
+#include "putvoxel.h"
+#include "cutvoxel.h"
 
 using namespace std;
 
@@ -48,6 +54,39 @@ int main()
             int x0, x1, y0, y1, z0, z1;
             ss>>x0>>x1>>y0>>y1>>z0>>z1;
             figs.push_back(new CutBox(x0, x1, y0, y1, z0, z1));
+        }
+        else if(s.compare("putsphere")==0){
+            int xcenter, ycenter, zcenter, radius;
+            float r, g, b, a;
+            ss>>xcenter>>ycenter>>zcenter>>radius>>r>>g>>b>>a;
+            figs.push_back(new PutSphere(xcenter, ycenter, zcenter, radius, r, g, b, a));
+        }
+        else if(s.compare("cutsphere")==0){
+            int xcenter, ycenter, zcenter, radius;
+            ss>>xcenter>>ycenter>>zcenter>>radius;
+            figs.push_back(new CutSphere(xcenter, ycenter, zcenter, radius));
+        }
+        else if(s.compare("putellipsoid")==0){
+            int xcenter, ycenter, zcenter, rx, ry, rz;
+            float r, g, b, a;
+            ss>>xcenter>>ycenter>>zcenter>>rx>>ry>>rz>>r>>g>>b>>a;
+            figs.push_back(new PutEllipsoid(xcenter, ycenter, zcenter, rx, ry, rz, r, g, b, a));
+        }
+        else if(s.compare("cutellipsoid")==0){
+            int xcenter, ycenter,  zcenter,  rx,  ry, rz;
+            ss>>xcenter>>ycenter>>zcenter>>rx>>ry>>rz;
+            figs.push_back(new CutEllipsoid(xcenter, ycenter, zcenter, rx, ry, rz));
+        }
+        else if(s.compare("putvoxel")==0){
+            int x, y, z;
+            float r, g, b, a;
+            ss>>x>>y>>z>>r>>g>>b>>a;
+            figs.push_back(new PutVoxel(x, y, z, r, g, b, a));
+        }
+        else if(s.compare("cutvoxel")==0){
+            int x, y, z;
+            ss>>x>>y>>z;
+            figs.push_back(new CutVoxel(x, y, z));
         }
 //        else {
 //            cout<<"Error reading file, program execution interupted!"<<endl<<"Please make sure your instructions are correctly under our guidelines before trying again."<<endl;
